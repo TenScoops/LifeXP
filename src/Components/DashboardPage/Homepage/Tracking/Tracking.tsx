9b94d6be889412d702697fa1cb3cca70bfc3ad4f
TenScoops <p.ayres9@gmail.com>
2024-06-11 03:07:36 -0400

    Tracking changes

--- src/Components/DashboardPage/Homepage/Tracking/Tracking.tsx
+++ src/Components/DashboardPage/Homepage/Tracking/Tracking.tsx
@@ -45,8 +45,7 @@ const Tracking: React.FC<TrackingProps> = ({setThePage}) => {
         { id: 1, icon: FitnessIcon, label: 'Complete 1 mile' },
         { id: 2, icon: WorkIcon, label: 'Complete a work task' },
         { id: 3, icon: PersonalIcon, label: 'Meditate 15 minutes' },
-        { id: 4, icon: WorkIcon, label: 'Complete a work task' },
-        { id: 5, icon: PersonalIcon, label: 'Journal thoughts' }
+    
     ];
 
     const styles = {
