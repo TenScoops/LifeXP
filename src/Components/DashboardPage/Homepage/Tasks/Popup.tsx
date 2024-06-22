bc45d6f652789f0c6380ec0080062dc8260b7220
TenScoops <p.ayres9@gmail.com>
2024-06-22 05:01:02 -0400

    popup changes

--- src/Components/DashboardPage/Homepage/Tasks/Popup.tsx
+++ src/Components/DashboardPage/Homepage/Tasks/Popup.tsx
@@ -8,16 +8,26 @@ const Popup = () => {
     <div className="border rounded-md border-black absolute
                  bottom-48 right-50 h-96 w-96 shadow-lg bg-white">
         
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col ">
             {/* close button */}
-            <h1>X</h1>
-            <h1>Task</h1>
-            <form className="ml-2">
-                <input onSubmit={onSubmit}
-                    className="border rounded-lg border-black h-32 w-80"/>
-            </form>
-            <h1>Choose Tag</h1>
-            <h1>Difficulty</h1>
+            <div className="flex justify-end mr-3">
+                <h1>X</h1>
+            </div>
+            <div className=" flex flex-col ">
+                <div className="flex ml-8">
+                    <h1>Task</h1>
+                </div>
+                
+                <form className="flex justify-center">
+                    <input onSubmit={onSubmit}
+                        className="border rounded-lg border-black h-32 w-80"/>
+                </form>
+                
+                <div className="ml-8">
+                    <h1>Choose Tag</h1>
+                    <h1>Difficulty</h1>
+                </div>
+            </div>
         </div>
 
     </div>)
